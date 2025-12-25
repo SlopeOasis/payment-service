bc52d734929aaa94b93d29241892245bc7c31864
AlesPozar <ales.pozar003@gmail.com>
2025-12-25 22:24:16 +0100

    setup za k8s za VM and stuff

--- src/main/java/com/slopeoasis/payment/config/WebConfig.java
+++ src/main/java/com/slopeoasis/payment/config/WebConfig.java
@@ -16,7 +16,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://frontend-navy-iota-66.vercel.app"
+                    )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
